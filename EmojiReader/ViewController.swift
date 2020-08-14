05f338c39e533421f96f7bf60c0a02662d4a24d7
17586317 <Borisov.S.Alek@sberbank.ru>
2020-08-15 00:21:47 +0300

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by 17586317 on 15.08.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
